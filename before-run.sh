7d518b43d0af8428883446536bd6eb05ed0f6e10
djonko <prevostdjonko@yahoo.fr>
2022-09-07 15:21:44 -0400

    add name

---
+++ before-run.sh
@@ -0,0 +1,5 @@
+cp ./searxng/settings.yml  /opt/data-adm-docker/volumes/searxng/data
+cp ./Caddyfile /opt/data-adm-docker/volumes/searxng/caddy
+sed -i "s|ultrasecretkey|$(openssl rand -hex 32)|g" /opt/data-adm-docker/volumes/searxng/data/settings.yml
+docker compose  --env-file /opt/data-adm-docker/volumes/searxng/.env  down
+docker compose --env-file /opt/data-adm-docker/volumes/searxng/.env up
